893487b46bf7ede4f5ac9692b8d6b1d4e787aefa
Emanoel Viana <59850170+emanoelvianads@users.noreply.github.com>
2024-06-03 09:05:51 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0b4fee3f6ebd388ed216ce599163a1b3",
+const RESOURCES = {"flutter_bootstrap.js": "6e6ec3e7ac0e4b7c903677573ad5c762",
 "version.json": "6605d368f80cf618e36a4a71b9edfdc6",
 "index.html": "687c5edfb8a33039cb6fc7c8317b38dc",
 "/": "687c5edfb8a33039cb6fc7c8317b38dc",
-"main.dart.js": "27cd7774b7bd47bc96051a006af79b88",
+"main.dart.js": "cf24d552e5955a1c13ddb5ef5f562c64",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
